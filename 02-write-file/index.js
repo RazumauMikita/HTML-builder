8e6bf1723ebd9cd99d939c921493d24a27a76237
Mikita <anykey2solo@gmail.com>
2023-05-06 11:02:11 +0300

    task 02 complete

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout, exit } = process; 
+const exitWord = 'exit';
+
+fs.writeFile(
+    path.join(__dirname, 'text.txt'),'' ,err => {
+        if (err) throw err;
+    }
+);
+console.log('Input text!');
+stdin.on('data', data => {
+   
+    const dataString = data.toString().trim();
+    if (dataString == exitWord || dataString == '^C') {
+        console.log('Bye-bye!');
+        exit();
+    }
+    fs.appendFile(
+        path.join(__dirname, 'text.txt'),
+        `${data}`,
+       (err) => {if (err) throw err}
+    )
+
+})
+
+process.on('SIGINT', () => {
+    console.log('Bye-bye!');
+    exit();
+})
