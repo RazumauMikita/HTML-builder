a2933a15b5d95efa4100017c68151f8a4c9cbe5a
Mikita <anykey2solo@gmail.com>
2023-05-06 20:08:33 +0300

    fix: fix 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,15 +2,28 @@ const fs = require('fs');
 const path = require('path');
 const fsPromises = require('fs/promises');
 
-fs.access(
+/*fs.rm(path.join(__dirname, 'files-copy'),
+    { recursive: true },
+    err => {}
+)*/
+fsPromises.mkdir(
     path.join(__dirname, 'files-copy'),
-    (err) => {
-        if (err) {
-            fsPromises.mkdir(
-                path.join(__dirname, 'files-copy'),
-                recursive = true
+    {recursive : true},
+    err => {
+        if (err) throw err; 
+    }
+)
+fs.readdir(
+    path.join(__dirname, 'files-copy'),
+    {withFileTypes: true},
+    (err, files) => {
+        if (err) throw err;
+        files.forEach((file) => {
+            fs.unlink(
+                path.join(__dirname, 'files-copy', file.name),
+                () => {}
             )
-        }
+        })
     }
 )
 fs.readdir(
@@ -29,3 +42,8 @@ fs.readdir(
         })
     }
 )
+
+/*fs.rmdir(
+    path.join(__dirname, 'dd'),
+    () => {}
+)*/
