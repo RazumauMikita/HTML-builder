265272240593824a8471fa0a9837171fdcaf012b
Mikita <anykey2solo@gmail.com>
2023-05-09 10:00:14 +0300

    fix

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-
 const { stdout } = process;
-fs.readFile(
-  path.join(__dirname, 'text.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err;
-    stdout.write(data);
-  },
-);
+const readStream = new fs.ReadStream(
+  path.join(__dirname, '/text.txt'),
+  'utf8',
+ );
+
+ readStream.on('data', (data) => {
+  stdout.write(data);
+ })
+
